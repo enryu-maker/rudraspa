1dabf63e6e6ae4185dd95d34eece3c8da230f7e4
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-24 23:46:38 +0530

    routing comleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
-function App() {
-  return <div className='flex items-center justify-center h-screen'>Hello</div>;
-}
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Services from './pages/Services';
+import Gallery from './pages/Gallery';
+import ContactUs from './pages/ContactUs';
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/about'
+          element={<AboutUs />}
+        />
+        <Route
+          path='/service'
+          element={<Services />}
+        />
+        <Route
+          path='/gallery'
+          element={<Gallery />}
+        />
+        <Route
+          path='/contact'
+          element={<ContactUs />}
+        />
+      </Routes>
+    </HashRouter>
+  );
+};
 
 export default App;
