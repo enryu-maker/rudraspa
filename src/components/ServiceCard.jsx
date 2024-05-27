22fd84620d72ea75a2e62bc8dd0ebd9c343e42a4
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-28 02:00:49 +0530

    cc

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const ServiceCard = ({ ...service }) => {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/service/${service.id}`);
-  };
-
   return (
     <>
-      <div className='flex flex-col gap-2 w-[350px]  shadow-lg rounded-md '>
+      <div className='flex flex-col gap-2 w-[400px] shadow-lg rounded-md '>
         <div>
           <img
-            src={service.image}
+            src={service?.image}
             alt=''
-            className='w-[350px] h-[200px] rounded-t-md px-0 object-cover'
+            className='w-[400px] h-[300px] rounded-t-md px-0 object-cover'
           />
         </div>
-        <div className='px-2 h-[80px] '>
+        <div className='px-2 h-[300px]  '>
           <h1 className='text-2xl p-2 font-Poppins  font-semibold text-primary-bg-color'>
-            {service.title}
+            {service?.title}
           </h1>
+          <p className=' p-2 text-pretty text-sm font-Poppins'>
+            {service?.description}
+          </p>
+          <div className='flex justify-between '>
+            <p className=' px-2 text-pretty text-md text-red-500 font-Poppins'>
+              Duration: {service?.duration}
+            </p>
+            <p className=' px-2 text-pretty text-md text-red-500 font-Poppins'>
+              Price: ₹ {service?.price}
+            </p>
+          </div>
         </div>
 
         <div className='px-2 py-2'>
-          <button
-            onClick={handleClick}
-            className='uppercase  bg-secondary-text-color font-Poppins text-white w-full p-2 rounded-md hover:bg-primary-bg-color hover:text-black hover:scale-95 transition-all duration-100 delay-75 ease-linear'>
-            Explore
+          <button className='uppercase  bg-secondary-text-color font-Poppins text-white w-full p-2 rounded-md hover:bg-primary-bg-color hover:text-black hover:scale-95 transition-all duration-100 delay-75 ease-linear'>
+            Book
           </button>
         </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -1,8 +1,19 @@
 // Image import here
-// import ThaiImg from './assets/thai.jpg';
-import AromaImg from './assets/aroma.jpg';
+import ThaiImg from './assets/thai.jpg';
+import HolisticSwedish from './assets/HolisticSwedish.jpg';
 import NeckAndBack from './assets/Swedish.jpg';
 import HeadMassage from './assets/headMassage.jpg';
+import FootReflexology from './assets/footR.jpg';
+import Aromatherapy from './assets/Aromatherapy.jpg';
+import Ballnese from './assets/Ballnese.jpg';
+import LomiLomi from './assets/LomiLomi.jpg';
+import SportsMassage from './assets/SportsMassage.jpg';
+import deepTissue from './assets/deepTissue.jpg';
+import RUDRASIGNATUREEXCLUSIVE from './assets/RUDRASIGNATUREEXCLUSIVE.jpg';
+import RUDRADESRTFLOWER from './assets/RUDRADESRTFLOWER.jpg';
+import RUDRACHOCOLATEBOOM from './assets/RUDRACHOCOLATEBOOM.jpg';
+import RelaxingCouple from './assets/RelaxingCouple.jpg';
+import PrinceCoupleMassage from './assets/PrinceCoupleMassage.jpg';
 
 export const ServiceData = [
   {
@@ -25,51 +36,132 @@ export const ServiceData = [
   },
   {
     id: 2,
-    title: 'Swedish Massage',
+    title: 'FOOT REFLEXOLOGY',
     description:
-      'A Swedish body massage has numerous health benefits. it helps to improve the physical as well as the mental health of the person receiving it. it calms the nervous system, strengthens muscles, tightenes the body`s loose muscles and improves blood flow.',
-    image: '',
+      'Foot Reflexology are often seen as a luxury rather than a necessity. But there`s way more to the practice than sheer relaxation and pampering. Foot Reflexology have a host of benefits to your overall health, making them less of a splurge-worthy extra and more of an overlooked healthy habit. Needless to say, they shouldn`t just be reserved for vacation massage tables and pedicure add-ons.',
+    image: FootReflexology,
+    duration: '60 minutes',
+    price: '1500',
   },
   {
     id: 3,
-    title: 'Balinese Massage',
+    title: 'HOLISTIC SWEDISH MASSAGE',
     description:
-      'Balinese massage is a full-body, deep-tissue, holistic treatment. Traditional Balinese massage combines gentle stretches, acupressure, reflexology and aromatherapy to stimulate the flow of blood, oxygen and energy around your body and bring a sense of wellbeing and deep relaxation.',
-    image: AromaImg,
+      'Holistic Swedish massage therapy promotes relaxation can help ease aches and patns. It helps to release body’s natural painkiller and gives a general sense of conteniment. this massage not only feels exceptionally good, but it is relaxing and invigorating. it is incredibly popular massage offering long lasting stress relief and a fantastic feeling of well being.',
+    image: HolisticSwedish,
+    duration: '60 minutes',
+    price: '1800',
   },
   {
     id: 4,
-    title: 'Deep Tissue Massage',
+    title: 'AROMATHERAPY',
     description:
-      'From Chronic pain managment to accelerating recovery of injuries the benefit of deep tissue massage are very here are some of them that release muscle tension relieves lower back pain, heal sports injuries, and increase mobility.',
-    image: AromaImg,
+      'Aromatherapy uses essential olls to make you feel calm, relaxed or energised. this oil work with your sense of smell , you can inhale them, bathe in them or be massaged with them.',
+    image: Aromatherapy,
+    duration: '60 minutes',
+    price: '1800',
   },
   {
     id: 5,
-    title: 'Four Hand Massage',
+    title: 'BALINESE MASSAGE',
     description:
-      '4 hand massage is an ancient technique of massage which involves two therapists working in tandem to deliver a simultaneous massage.',
-    image: AromaImg,
+      'Ballnese Massage Is one from of traditional Indonesian massage. originated on the island of bali, this massage involves a combination of techniques, including massage, acupressure, reflexology, can also help loosen muscles and ease pain.',
+    image: Ballnese,
+    duration: '60 minutes',
+    price: '2200',
   },
   {
     id: 6,
-    title: 'Refloxology Massage',
+    title: 'LOMI LOMI',
     description:
-      'This service can greatly benefit the flows and regulators of your body to aid in them functioning. It`s a simple yet very effective therapy that everyone should try at least once! You never know; it could become your favorite massage technique..',
-    image: AromaImg,
+      'It ls a unique massage derived from the anclent polyneslans and more specially the master healer of hawaiian island, this massage is performed using forearms, knuckles and palms. it is specially designed to relieve mental and physical stress.',
+    image: LomiLomi,
+    duration: '60 minutes',
+    price: '2499',
   },
   {
     id: 7,
-    title: 'Signature Massage',
+    title: 'SPORTS MASSAGE',
     description:
-      'Stimulate and de-stress yourself with our signature body massage in Rudra Spa In Powai to relieve your muscle ache pressure, alleviate fatigue and promote blood circulation, leaving you feeling refreshed and rejuvenated, which requires the essence of relaxation? Signature massage in our makes you relax mentally.',
-    image: AromaImg,
+      'Sports massage Is a form of bodywork geared toward participants In athletics. It is used to help prevent injuries, to prepare the body for athletic activity and maintain it in optimal condition, and to help athletes recover from workouts and injuries. Sports massage has three basic forms: pre-event massage, post-event massage, and maintenance massage.',
+    image: SportsMassage,
+    duration: '60 minutes',
+    price: '2599',
   },
   {
     id: 8,
-    title: 'Couple Massage',
+    title: 'DEEP TISSUE MASSAGE',
     description:
-      'A couples Massage is designed for two people to enjoye together, mening that any two people are welcome to schedule a session. we have unique tratment rooms that are set up with two separate massage tables so that each clint can be massaged simultaneously by two different therapists.',
-    image: AromaImg,
+      'Deep tissue massage Is a massage technique that`s mainly used to treat musculoskeletal issues, such as strains and sports injuries. It involves applying sustained pressure using slow, deep strokes to target the inner layers of your muscles and connective tissues. This helps to break up scar tissue that forms following an injury and reduce tension in muscle and tissue. It may also promote faster healing by increasing blood flow and reducing inflammation',
+    image: deepTissue,
+    duration: '60 minutes',
+    price: '2599',
+  },
+  {
+    id: 9,
+    title: 'THAI MASSAGE',
+    description:
+      'In thai Massage therapist use their hands, thumbs, elbow, forearms and som times even their feet to reduce tension in your muscles. thai massage is based on the idea that energy moves through your body predictable lines or pathways known as sens. thai massage can benefits your health in numerous way Ilke rellves headaches, reduces back pain, relleves Joints stiffness and pain, increase flexibility and range of motion, eases anxiety, revives Energy.',
+    image: ThaiImg,
+    duration: '60 minutes',
+    price: '1500',
+  },
+];
+
+export const RudraSignatureMassage = [
+  {
+    id: 0,
+    title: 'RUDRA SIGNATURE EXCLUSIVE',
+    description:
+      'A complete rejuvenating a massage, which hydrates, exfoliates and 6 . nourished the body whllst contouring It at the same time. The tissue  and fatty areas that are kneaded into shape. Subsequent friction with worming scrubs, hot towels and a hot mild soak re-texturize the skin. Hydrotherapy aids the detoxification process.',
+    image: RUDRASIGNATUREEXCLUSIVE,
+    duration: '60 minutes',
+    price: '3200',
+  },
+  {
+    id: 1,
+    title: 'RUDRA DESRT FLOWER',
+    description:
+      'A complete rejuvenating a massage, which hydrates, exfoliates and 6 . nourished the body whllst contouring It at the same time. The tissue  and fatty areas that are kneaded into shape. Subsequent friction with worming scrubs, hot towels and a hot mild soak re-texturize the skin. Hydrotherapy aids the detoxification process.',
+    image: RUDRADESRTFLOWER,
+    duration: '90 minutes',
+    price: '3800',
+  },
+  {
+    id: 2,
+    title: 'RUDRA CHOCOLATE BOOM',
+    description:
+      'After years of being incorrectly blamed for causing acne, chocolate has redeemed itself as the new skin savior at spas around the world. Classic confection brands . often market thelr own massage creams and treatments alongside off-brands with a basic cocoa base. Chocolate spa treatments include basic massages with a cocoa-based cream or oil, hot oil rubs, chocolate waxes and facials, while commercial products Incorporate cocoa Into everything from hand creams to lip gloss.',
+    image: RUDRACHOCOLATEBOOM,
+    duration: '90 minutes',
+    price: '4500',
+  },
+];
+export const CoupleMassage = [
+  {
+    id: 0,
+    title: 'RELAXING COUPLE MASSAGE',
+    Women: 'WOMEN: JASMIN MASSAGE OIL',
+    WomenDescription:
+      'Massage reduce the stress hormones cortisol In the body which helps fo lift Duration - 90 minutes moods and lower blood pressure. the aromatic effect of jasmine oil stimulates the release of certain hormones in the body including serotonin, which result Massage Steam, Srcub & Shower In the boost of energy and the uplifted mood.',
+    Men: 'MEN: SANDALWOOD MASSAGE OIL',
+    MenDescription:
+      'Take care of your body it the only place have to live. sandalwood massage oil is done that can help you to achieve more clarity and calmness due to its extensive therapeutic benefits. the essential oil improve and maintain good nerve health help heal by promoting growth of new tissue.',
+    image: RelaxingCouple,
+    duration: '90 minutes',
+    price: '3650',
+  },
+  {
+    id: 1,
+    title: 'PRINCE / PRINCESS HEAVENLY TOUCH ',
+    Women: 'WOMEN: CHOCOLATE PUMICE SCRUB',
+    WomenDescription:
+      'Crave out sometime for a massage with chocolate pumice scrub which is Massage Steam, Srcub & Shower made of pumice scrub in a cream base which provides scrubbing and molsturlzing.',
+    Men: 'MEN : CHOCOLATE PUMICE SCRUB',
+    MenDescription:
+      'Energize your body, stimulate your mind and calm your soul with organic sugar scrub. using a sugar for exfoliation involves rubbing the sugar against your skin and the pressure.',
+    image: PrinceCoupleMassage,
+    duration: '90 minutes',
+    price: '3650',
   },
 ];
