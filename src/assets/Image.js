9eec0b62fff48f54858f524f4e58991e93f6daed
enryu-maker <akifkhan60067@gmail.com>
2024-06-13 13:34:25 +0530

    cc

--- src/assets/Image.js
+++ src/assets/Image.js
@@ -1,4 +1,5 @@
 export const Image = {
     b1 : require('./Rudra Hammam Spa.png'),
     partner : require('./patner.png'),
+    nt : require('../assets/Footer (400 x 100 px).png')
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { NavLinkData } from './Header';
 import { NavLink } from 'react-router-dom';
+import { Image } from '../assets/Image';
 
 const OurService = [
   {
-    title: 'Ayurvedic Therapy',
+    title: 'Hammam Massage',
     path: '/service',
   },
   {
-    title: 'Scrubs & Masks',
+    title: 'Aroma',
     path: '/service',
   },
   {
-    title: 'Spa Treatments',
+    title: 'Swedish',
     path: '/service',
   },
   {
-    title: 'Ayurvedic Treatments',
-    path: '/v',
-  },
-  {
-    title: 'Ayurvedic Treatments',
+    title: 'Deep tissue',
     path: '/service',
   },
 ];
@@ -28,30 +25,14 @@ const OurService = [
 const Footer = () => {
   return (
     <div className='container bg-primary-bg-color mx-auto pt-20'>
-      <div className='max-w-7xl mx-auto'>
-      <div className='grid grid-cols-1  sm:grid-cols-3 gap-10 min-h-[50vh]   opacity-70 px-5'>
-      
-        <div className='flex flex-col gap-5 items-start'>
-          <h1 className='text-2xl font-Poppins font-bold text-secondary-text-color uppercase'>
-            Important Links
-          </h1>
-          {NavLinkData.map(({ title, path }) => (
-            <NavLink
-              key={title}
-              to={path}
-              className='uppercase font-semibold font-Poppins text-lg text-black hover:text-white'>
-              {title}
-            </NavLink>
-          ))}
-        </div>
+      <div className='max-w-7xl mx-auto flex flex-col justify-evenly items-center'>
+        <div className='grid grid-cols-1  sm:grid-cols-3 gap-10 min-h-[50vh]    opacity-70 px-5'>
 
-
-        <div className='flex flex-col gap-5 items-start'>
-          <h1 className='text-2xl font-Poppins font-bold text-secondary-text-color uppercase'>
-            Our Services
-          </h1>
           <div className='flex flex-col gap-5 items-start'>
-            {OurService.map(({ title, path }) => (
+            <h1 className='text-2xl font-Poppins font-bold text-secondary-text-color uppercase'>
+              Important Links
+            </h1>
+            {NavLinkData.map(({ title, path }) => (
               <NavLink
                 key={title}
                 to={path}
@@ -60,25 +41,37 @@ const Footer = () => {
               </NavLink>
             ))}
           </div>
-        </div>
 
-        
-        <div className='flex flex-col gap-5 items-start'>
-          <h1 className='text-2xl font-Poppins font-bold text-secondary-text-color uppercase'>
-            Franchise
-          </h1>
+
           <div className='flex flex-col gap-5 items-start'>
-            {OurService.map(({ title, path }) => (
-              <NavLink
-                key={title}
-                to={path}
-                className='uppercase font-semibold font-Poppins text-lg text-black hover:text-white'>
-                {title}
-              </NavLink>
-            ))}
+            <h1 className='text-2xl font-Poppins font-bold text-secondary-text-color uppercase'>
+              Our Services
+            </h1>
+            <div className='flex flex-col gap-5 items-start'>
+              {OurService.map(({ title, path }) => (
+                <NavLink
+                  key={title}
+                  to={path}
+                  className='uppercase font-semibold font-Poppins text-lg text-black hover:text-white'>
+                  {title}
+                </NavLink>
+              ))}
+            </div>
           </div>
+
+
+          <div className='flex flex-col gap-5 items-start'>
+            <h1 className='text-2xl font-Poppins font-bold text-secondary-text-color uppercase'>
+              Franchise Contact
+            </h1>
+            <div className='flex flex-col gap-5 items-start'>
+              <a className='uppercase font-semibold font-Poppins text-lg text-black hover:text-white' href="">number</a>
+              <a className='uppercase font-semibold font-Poppins text-lg text-black hover:text-white' href="">email</a>
+
+            </div>
+          </div>
+          <img src={Image.nt} className='self-center' />
         </div>
-      </div>
       </div>
     </div>
   );
